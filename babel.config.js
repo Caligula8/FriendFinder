5ba43a580c863d0ee44a7b00ffdba450b807dec5
Caligula8 <ajucan8.spam@gmail.com>
2024-02-14 08:22:54 -0500

    fixed issue

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,13 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo", "@babel/preset-env"],
-    plugins: ["nativewind/babel"],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      ["@babel/plugin-transform-runtime", { regenerator: true }],
+      ["@babel/plugin-transform-class-properties", { loose: true }],
+      ["@babel/plugin-transform-private-methods", { loose: true }],
+      ["@babel/plugin-transform-private-property-in-object", { loose: true }],
+      "nativewind/babel",
+    ],
   };
 };
